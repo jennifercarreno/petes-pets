60d210b1f0754e03d173523bd314b5942562ec30
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-05-25 18:05:37 -0700

    users can search by name and breed

--- routes/pets.js
+++ routes/pets.js
@@ -60,7 +60,10 @@ module.exports = (app) => {
   app.get('/search', (req, res) => {
     term = new RegExp(req.query.term, 'i')
   
-    Pet.find({'name': term}).exec((err, pets) => {
+    Pet.find({$or:[
+      {'name': term},
+      {'species': term}
+    ]}).exec((err, pets) => {
       res.render('pets-index', { pets: pets });
     })
   });

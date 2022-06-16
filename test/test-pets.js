249becd8911ada9e795f8df6395d8fb51a722a26
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-06-16 14:50:37 -0700

    implemented responding to json

--- test/test-pets.js
+++ test/test-pets.js
@@ -128,3 +128,15 @@ it('should search ALL pets by name on /search GET', (done) => {
       });
 });
 });
+
+it('should list ALL pets on /pets GET', function(done) {
+  chai.request(server)
+      .get('/')
+      .set('content-type', 'application/json')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
+});
